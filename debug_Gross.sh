874300365b5ff2fa740cf648e73f465a55809a63
Iwo Gross <ipg0004@auburn.edu>
2019-02-19 17:16:36 -0600

    Added the _setbp and _clearbp scripts to debug_GROSS.sh

--- debug_Gross.sh
+++ debug_Gross.sh
@@ -108,3 +108,36 @@ esac
 done
 }
 
+# Set a breakpoint at the given line number or list breakpoints
+function _setbp
+{
+local i
+if [ -z "$1" ]; then
+_listbp
+elif [ $(echo $1 | grep '^[0-9]*') ]; then
+if [ -n "${_lines[$1]}" ]; then
+_linebp=($(echo $( (for i in ${_linebp[*]} $1; do
+echo $i; done) | sort -n) ))
+_msg "Breakpoint set at line $1"
+else
+_msg "Breakpoints can only be set on non-blank lines"
+fi
+else
+_msg "Please specify a numeric line number"
+fi
+}
+
+function _clearbp
+{
+local i
+if [ -z "$1" ]; then
+unset _linebp[*]
+_msg "All breakpoints have been cleared"
+elif [ $(echo $1 | grep '^[0-9]*') ]; then
+_linebp=($(echo $(for i in ${_linebp[*]}; do
+if (( $1 != $i )); then echo $i; fi; done) ))
+_msg "Breakpoint cleared at line $1"
+else
+_msg "Please specify a numeric line number"
+fi
+}
